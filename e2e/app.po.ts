0a6428fb1ea3a5018e8ce0af62292df76b56c26c
Angular CLI <angular-cli@angular.io>
2018-03-07 13:49:09 +0800

    chore: initial commit from @angular/cli

---
+++ e2e/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('bluva-root h1')).getText();
+  }
+}
